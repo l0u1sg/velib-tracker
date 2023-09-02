752a1f8b326906617871ffb8e63c4ee0ad0cd773
Louis <me@louisgallet.fr>
2023-09-02 14:24:53 +0200

    Initial Commit

---
+++ velibtracker/velibtrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  velibtrackerApp.swift
+//  velibtracker
+//
+//  Created by Louis Gallet on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct velibtrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
